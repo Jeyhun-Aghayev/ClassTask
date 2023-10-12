3569546763dfd0cfaf990b9faa6e57e2a3b09dff
Jeyhun-Aghayev <145680779+Jeyhun-Aghayev@users.noreply.github.com>
2023-10-12 13:34:25 +0400

    Add files via upload

---
+++ CeyhunTask/Phone.cs
@@ -0,0 +1,89 @@
+﻿class Phone
+{
+    private int _id;
+    private string _name;
+    private string _brandname;
+    private double _price;
+    private int _count;
+
+
+    public int Id 
+    {
+        get
+        {
+            return _id;
+        }
+        set
+        {
+            if (value>0) 
+            {
+            _id = value;
+            }
+        }
+    }
+    public string Name
+    {
+        get
+        {
+            return _name;
+        }
+        set
+        {
+            value = value.Trim();
+            if (value.Length > 1)
+            {
+                _name = value;
+            }
+
+        }
+    }
+
+    public string BrandName {
+        get
+        {
+            return _brandname;
+        }
+        set
+        {
+            value=value.Trim();
+            if (value.Length > 2)
+            {
+            _brandname=value;
+            }
+
+        }
+    }
+    public double Price
+    {
+        get
+        {
+            return _price;
+        }
+        set
+        {
+            if (value > 0)
+            {
+                _price = value;
+            }
+        }
+    }
+    public int Count
+    {
+        get
+        {
+            return _count;
+        }
+        set
+        {
+            if (value > 0)
+            {
+                _count = value;
+            }
+        }
+    }
+
+    public void ShowInfo()
+    {
+        Console.WriteLine($"ID: {Id} \nName: {Name}\nBrand: {BrandName}\nPrice: {Price}\nCount: {Count}");
+    }
+}
---
+++ CeyhunTask/Program.cs
@@ -0,0 +1,30 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Store myStore = new Store { Name = "My Store" };
+
+        Phone phone1 = new Phone { Id = 1, Name = "13 pro", BrandName = "Iphone", Price = 1998.99, Count = 10 };
+        Phone phone2 = new Phone { Id = 2, Name = "13 pro max", BrandName = "Iphone", Price = 1799.99, Count = 5 };
+        Phone phone3 = new Phone { Id = 3, Name = "14 pro", BrandName = "Iphone", Price = 2498.99, Count = 10 };
+        Phone phone4 = new Phone { Id = 4, Name = "14 pro max", BrandName = "Iphone", Price = 2998.99, Count = 10 };
+
+
+        myStore.AddPhone(phone1);
+        myStore.AddPhone(phone2);
+        myStore.AddPhone(phone3);
+        myStore.AddPhone(phone4);
+
+        myStore.ShowAllPhones();
+        Console.WriteLine("\n-------------------------------------------------------------------------------------------------------");
+        double minPrice = 1000.00;
+        double maxPrice = 2000.00;
+        myStore.ShowPhonesForPrice(minPrice, maxPrice);
+        myStore.RemovePhone(2);
+        Console.WriteLine("\n-------------------------------------------------------------------------------------------------------");
+        Console.WriteLine("FromRemove:");
+        myStore.ShowAllPhones();
+
+
+    }
+}
